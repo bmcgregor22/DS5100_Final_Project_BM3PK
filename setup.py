0c02fac7a425d1377bab1ea682afff4f584a735d
Bruce A MCGREGOR <111203887+bmcgregor22@users.noreply.github.com>
2022-11-30 16:17:41 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup (name='montecarlo',
+      version = '1.0.0',
+      description='monte carlo simulator',
+      url='https://github.com/bmcgregor22/DS5100_Final_Project_BM3PK.git',
+      author = 'Bruce McGreor',
+      license = 'MIT',
+      packages= ['src','tests'])
+       
+       
