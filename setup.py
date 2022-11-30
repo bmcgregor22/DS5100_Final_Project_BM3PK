ce4ce203bafd823d36658cd90eb272713d82c8a7
Bruce A MCGREGOR <111203887+bmcgregor22@users.noreply.github.com>
2022-11-30 16:21:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup (name='montecarlo',
       url='https://github.com/bmcgregor22/DS5100_Final_Project_BM3PK.git',
       author = 'Bruce McGreor',
       license = 'MIT',
-      packages= ['src','tests'])
+      packages= ['src','simulator'])
        
        
